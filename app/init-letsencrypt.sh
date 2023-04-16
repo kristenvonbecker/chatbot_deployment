860c961e39bd89bd8e45ef06b233013e114efd53
kristenvonbecker <71301766+kristenvonbecker@users.noreply.github.com>
2023-04-15 18:36:24 -0700

    Updates to volumes

--- app/init-letsencrypt.sh
+++ app/init-letsencrypt.sh
@@ -55,7 +55,7 @@ esac
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
 docker compose run --rm --entrypoint "certbot certonly \
-  --webroot -w /var/www/certbot \
+  --webroot -w /var/www \
   $staging_arg \
   $email_arg \
   $domain_args \
