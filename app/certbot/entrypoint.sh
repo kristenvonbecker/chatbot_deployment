8faf5dc5ff0f3b543a9c462c60e609fe5c2dc221
kristenvonbecker <71301766+kristenvonbecker@users.noreply.github.com>
2023-04-17 13:56:42 -0700

    Update config

--- app/certbot/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-trap exit TERM
-while true
-do
-  certbot renew
-  sleep 12h & wait
-done
--- app/web/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-while true
-do
-  sleep 6h & wait
-  nginx -s reload
-done &
-nginx -g 'daemon off;'
